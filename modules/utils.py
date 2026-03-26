1c5f2e95b5026c58450bf592a92f2ae7d970e713
Francesc <cesc.dovi@gmail.com>
2026-03-26 18:59:28 +0100

    matmul_biasses improved using numpy

--- modules/utils.py
+++ modules/utils.py
@@ -1,11 +1,11 @@
-#PISTA: es esta la mejor forma de hacer una matmul?
+import numpy as np
+
+# optimizar multiplicacion de matrices usando numpy (que está hecho en C)
 def matmul_biasses(A, B, C, bias):
-    m, p, n = A.shape[0], A.shape[1], B.shape[1]
-    
-    for i in range(m):
-        for j in range(n):
-            for k in range(p):
-                C[i][j] += A[i][k] * B[k][j]
-            C[i][j] += bias[j]
-    return C
+    result = np.asarray(A, dtype=np.float32) @ np.asarray(B, dtype=np.float32)
+    result += np.asarray(bias, dtype=np.float32)
+
+    if C is not None:
+        C= result
+    return result
 
