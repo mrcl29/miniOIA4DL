22a1e8c90653508e9900e6374a406bb5088b973f
Francesc <cesc.dovi@gmail.com>
2026-03-28 11:32:29 +0100

    Maxpool2d improved using numpy

--- modules/maxpool2d.py
+++ modules/maxpool2d.py
@@ -1,6 +1,7 @@
 from modules.layer import Layer
 #from cython_modules.maxpool2d import maxpool_forward_cython
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 
 class MaxPool2D(Layer):
     def __init__(self, kernel_size, stride):
@@ -8,45 +9,47 @@ class MaxPool2D(Layer):
         self.stride = stride
 
     def forward(self, input, training=True):  # input: np.ndarray of shape [B, C, H, W]
-        self.input = input
-        B, C, H, W = input.shape
+        self.input = np.asarray(input, dtype=np.float32)
+        B, C, H, W = self.input.shape
         KH, KW = self.kernel_size, self.kernel_size
         SH, SW = self.stride, self.stride
 
         out_h = (H - KH) // SH + 1
         out_w = (W - KW) // SW + 1
 
-        self.max_indices = np.zeros((B, C, out_h, out_w, 2), dtype=int)
-        output = np.zeros((B, C, out_h, out_w),dtype=input.dtype)
+        # Extraer todas las ventanas de una vez: (B, C, out_h, out_w, KH, KW)
+        windows = sliding_window_view(self.input, (KH, KW), axis=(2, 3))[:, :, ::SH, ::SW]
+        flat = windows.reshape(B, C, out_h, out_w, KH * KW)
 
-        for b in range(B):
-            for c in range(C):
-                for i in range(out_h):
-                    for j in range(out_w):
-                        h_start = i * SH
-                        h_end = h_start + KH
-                        w_start = j * SW
-                        w_end = w_start + KW
+        # Índice plano del máximo en cada ventana
+        argmax = np.argmax(flat, axis=-1)          # (B, C, out_h, out_w)
 
-                        window = input[b, c, h_start:h_end, w_start:w_end]
-                        max_idx = np.unravel_index(np.argmax(window), window.shape)
-                        max_val = window[max_idx]
+        # Convertir índice plano a coordenadas absolutas
+        h_off = argmax // KW
+        w_off = argmax % KW
+        h_base = (np.arange(out_h) * SH).reshape(1, 1, out_h, 1)
+        w_base = (np.arange(out_w) * SW).reshape(1, 1, 1, out_w)
 
-                        output[b, c, i, j] = max_val
-                        self.max_indices[b, c, i, j] = (h_start + max_idx[0], w_start + max_idx[1])
+        self.max_indices = np.empty((B, C, out_h, out_w, 2), dtype=np.intp)
+        self.max_indices[..., 0] = h_base + h_off
+        self.max_indices[..., 1] = w_base + w_off
 
-        return output
+        output = np.take_along_axis(flat, argmax[..., None], axis=-1)[..., 0]
+        return output.astype(np.float32, copy=False)
 
     def backward(self, grad_output, learning_rate=None):
+        grad_output = np.asarray(grad_output, dtype=np.float32)
         B, C, H, W = self.input.shape
-        grad_input = np.zeros_like(self.input, dtype=grad_output.dtype)
         out_h, out_w = grad_output.shape[2], grad_output.shape[3]
+        grad_input = np.zeros((B, C, H, W), dtype=np.float32)
 
-        for b in range(B):
-            for c in range(C):
-                for i in range(out_h):
-                    for j in range(out_w):
-                        r, s = self.max_indices[b, c, i, j]
-                        grad_input[b, c, r, s] += grad_output[b, c, i, j]
+        # Scatter-add vectorizado: propaga gradiente solo a la posición del máximo
+        b_idx = np.broadcast_to(np.arange(B)[:, None, None, None], (B, C, out_h, out_w))
+        c_idx = np.broadcast_to(np.arange(C)[None, :, None, None], (B, C, out_h, out_w))
+        np.add.at(
+            grad_input,
+            (b_idx, c_idx, self.max_indices[..., 0], self.max_indices[..., 1]),
+            grad_output,
+        )
 
         return grad_input
