1fbfa9f2ce85a4d87b0ca0b1926116cfd0c75434
Francesc <cescdovi@gmail.com>
2026-04-08 08:57:08 +0200

    use_gpu flag added

--- models/oianet_cifar100.py
+++ models/oianet_cifar100.py
@@ -10,30 +10,30 @@ from modules.dropout import Dropout
 
 
 class OIANET_CIFAR100(BaseModel):
-    def __init__(self, conv_algo=0):
+    def __init__(self, conv_algo=0, use_gpu=False):
         print("Building OIANet for CIFAR-100")
         layers = [
-            Conv2D(3, 32, kernel_size=3, stride=1, padding=1, conv_algo=conv_algo),
-            BatchNorm2D(32),
-            ReLU(),
-            MaxPool2D(kernel_size=2, stride=2),
+            Conv2D(3, 32, kernel_size=3, stride=1, padding=1, conv_algo=conv_algo, use_gpu=use_gpu),
+            BatchNorm2D(32, use_gpu=use_gpu),
+            ReLU(use_gpu=use_gpu),
+            MaxPool2D(kernel_size=2, stride=2, use_gpu=use_gpu),
 
-            Conv2D(32, 64, kernel_size=3, stride=1, padding=1, conv_algo=conv_algo),
-            BatchNorm2D(64),
-            ReLU(),
-            MaxPool2D(kernel_size=2, stride=2),
+            Conv2D(32, 64, kernel_size=3, stride=1, padding=1, conv_algo=conv_algo, use_gpu=use_gpu),
+            BatchNorm2D(64, use_gpu=use_gpu),
+            ReLU(use_gpu=use_gpu),
+            MaxPool2D(kernel_size=2, stride=2, use_gpu=use_gpu),
 
-            Conv2D(64, 128, kernel_size=3, stride=1, padding=1, conv_algo=conv_algo),
-            BatchNorm2D(128),
-            ReLU(),
-            MaxPool2D(kernel_size=2, stride=2),
+            Conv2D(64, 128, kernel_size=3, stride=1, padding=1, conv_algo=conv_algo, use_gpu=use_gpu),
+            BatchNorm2D(128, use_gpu=use_gpu),
+            ReLU(use_gpu=use_gpu),
+            MaxPool2D(kernel_size=2, stride=2, use_gpu=use_gpu),
 
-            Flatten(),
-            Dense(128 * 4 * 4, 256),
-            ReLU(),
-            Dropout(0.5),  # Dropout layer with 50% probability
+            Flatten(use_gpu=use_gpu),
+            Dense(128 * 4 * 4, 256, use_gpu=use_gpu),
+            ReLU(use_gpu=use_gpu),
+            Dropout(0.5, use_gpu=use_gpu),
 
-            Dense(256, 100),
-            Softmax()
+            Dense(256, 100, use_gpu=use_gpu),
+            Softmax(use_gpu=use_gpu)
         ]
         super().__init__(layers)
